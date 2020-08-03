d78e00b162f4ffc2b85a91268ee50d96d53a8ab6
kobeliu0919 <69131746+kobeliu0919@users.noreply.github.com>
2020-08-03 11:14:27 +0800

    Add files via upload

---
+++ Day 1-1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:06:14 2020
+
+@author: USER
+"""
+
+print("I’m Eric \nHe is “cool”")
---
+++ Day 1-2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:51 2020
+
+@author: USER
+"""
+name=input("please enter your name:\n")
+print("Hi "+name)
