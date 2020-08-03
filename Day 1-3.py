e879557677145a0c7ad464e53dd4cc550f27d2fb
kobeliu0919 <69131746+kobeliu0919@users.noreply.github.com>
2020-08-03 11:56:07 +0800

    Add files via upload

---
+++ Day 1-3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:24:35 2020
+
+@author: USER
+"""
+
+a=input("enter a number:\n")
+b=float(a)
+r=b*2*3.14
+r2=b*b*3.14
+print("圓周:",r)
+print("面積:",r2)
